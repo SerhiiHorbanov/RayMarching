7eff19b33c768afda07880a61b05c274c270d566
Serhii <serikgorbanev@gmail.com>
2024-01-21 18:08:14 +0100

    Added project

---
+++ RayMarching/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RayMarching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
