75877e85d3e3782403b08e476192693c47d6772b
Serhii <serikgorbanev@gmail.com>
2024-01-22 08:18:49 +0100

    Added RayMarchingShape class

---
+++ RayMarching/Render/RayMarchingShape.cs
@@ -0,0 +1,11 @@
+﻿using SFML.System;
+
+namespace RayMarching.Render
+{
+    abstract class RayMarchingShape
+    {
+        public Vector3f position;
+
+        public abstract float calculateDistanceTo(Vector3f otherPosition);
+    }
+}
