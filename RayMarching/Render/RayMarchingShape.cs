c393a9d4d247d5fdb89a2d7510c91ec822dbbd53
Serhii <serikgorbanev@gmail.com>
2024-01-23 05:27:54 +0100

    Added color and constructor to RayMarchingShape

--- RayMarching/Render/RayMarchingShape.cs
+++ RayMarching/Render/RayMarchingShape.cs
@@ -1,10 +1,18 @@
-﻿using SFML.System;
+﻿using SFML.Graphics;
+using SFML.System;
 
 namespace RayMarching.Render
 {
     abstract class RayMarchingShape
     {
         public Vector3f position;
+        public Color color;
+
+        protected RayMarchingShape(Vector3f position, Color color)
+        {
+            this.position = position;
+            this.color = color;
+        }
 
         public abstract float calculateDistanceTo(Vector3f otherPosition);
     }
